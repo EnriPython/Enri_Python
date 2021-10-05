09d607fe45ddf1a07f32e59f143554cb5a545b63
Enrique <58607391+EnriPython@users.noreply.github.com>
2021-10-05 11:31:22 +0100

    Update oper_polinomios.py

--- oper_polinomios.py
+++ oper_polinomios.py
@@ -2,34 +2,48 @@
 # -*- coding: utf-8 -*-
 
 
-# Importamos las libreria sympy, sys y string
+# Importamos las libreria SympY, sys y string
 
 import sympy
 from sympy import *
 import sys
 import string
 
+
 sys.displayhook = pprint 
+
 
 # Definimos los simbolos
 
 sympy.init_printing()
-x,y = sympy.symbols('x,y')
+x, y = sympy.symbols('x, y')
 
 
-print"     #######################################################################"
-print"     #                                                                     #"
-print"     #   Algoritmo para la resolucion de operaciones entre 2 polinomios    #"
-print"     #                                                                     #"
-print"     #      multiplicacion, suma, resta, division, factorial y raiz        #"
-print"     #                                                                     #"
-print"     #                        Enri_Python v1.0                             #"
-print"     #                                                                     #"
-print"     #        Exprese el polinomio de ejemplo: 7x³ + 2x² + x - 7           #"
-print"     #                                                                     #"
-print"     #            bajo este formato: 7*x**3 + 2*x**2 + x - 7               #"
-print"     #                                                                     #"
-print"     #######################################################################"
+# evita que el programa se cierre en windows ante algun error de salida
+
+def show_exception_and_exit(exc_type, exc_value, tb):
+    import traceback
+    traceback.print_exception(exc_type, exc_value, tb)
+    raw_input("Presione <ENTER> para salir ------->")
+    sys.exit(-1)
+
+
+sys.excepthook = show_exception_and_exit
+
+
+print"     ##########################################################################"
+print"     #                                                                        #"
+print"     #    Algoritmo para la resolucion de operaciones entre 2 polinomios      #"
+print"     #                                                                        #"
+print"     #       multiplicacion, suma, resta, division, factorial y raiz          #"
+print"     #                                                                        #"
+print"     #                          Enri_Python v2.0                              #"
+print"     #                                                                        #"
+print"     #  Exprese el polinomio de ejemplo: 7x(elevado)3 + 2x(elevado)2 + x - 7  #"
+print"     #                                                                        #"
+print"     #            bajo este formato: 7*x**3 + 2*x**2 + x - 7                  #"
+print"     #                                                                        #"
+print"     ##########################################################################"
 print""
 
 # Entrada de polinomios 
@@ -47,22 +61,26 @@ Poly2 = sympy.Poly(p2)
 # Declaramos una funcion para cada operacion que queramos utilizar
 
 def mult(p1, p2):
-	return p1 * p2
+    return p1 * p2
+
 
 def suma(p1, p2):
-	return p1 + p2
+    return p1 + p2
+
 
 def res(p1, p2):
-	return p1 - p2
+    return p1 - p2
+
 
 def cociente(p1, p2):
-	try:
-		C,R = div(p1, p2)
-		return [C, R]
-	except:
-		return "      El segundo polinomio no puede ser nulo" 
+    try:
+        C, R = div(p1, p2)
+        return [C, R]
+    except:
+        return "el segundo polinomio no puede ser nulo"
 
-C,R = cociente(p1, p2)
+
+C, R = cociente(p1, p2)
 
 
 # Guardamos los valores retornados por las funciones y les pasamos los 2 polinomios como parametros,  Poly1 y Poly2
@@ -74,7 +92,7 @@ resultDiv = C
 resultRest = R
 
 
-# Mostramos el valor que queremos
+# Mostramos el valor que deseemos
 
 print"     El resultado de la multiplicacion es: ", resultMult
 print""
@@ -88,102 +106,96 @@ print"     El resto de la division es: ", resultRest
 print""
 print""
 
-# Comprobamos que el simbolo <x> esta presente en el polinomio
- 
-if string.find(Poly1, "x"):
 
+# Comprobamos que el simbolo <x> esta presente en el polinomio
 # Factoriza primer polinomio introducido y raiz
 
-	p = sympy.simplify(Poly1)
-	p_exp = p.expand()
-	resultFactor = p_exp.factor()
-	resuligualCero = solve(p, 'x')
+if string.find(Poly1, "x"):
+    pa = sympy.simplify(p1)
+    pa_exp = pa.expand()
+    resultFactor = pa_exp.factor()
+    resuligualCero = solve(pa, 'x')
 
-	print"     El primer polinomio factorizado es: ", resultFactor
-	print"     El primer polinomio igulado a cero da como raices de <x>: ", resuligualCero
-	print""
-	print""
+    print"     El primer polinomio factorizado es: ", resultFactor
+    print"     El primer polinomio igulado a cero da como raices de <x>: ", resuligualCero
+    print""
+    print""
 
-
-if string.find(Poly2, "x"):
 
 # Factoriza el segundo polinomio introducido y raiz
 
-	p2 = sympy.simplify(Poly2)
-	p2_exp = p2.expand()
-	resultFactorp2 = p2_exp.factor()
-	resuligualCerop2 = solve(p2, 'x')
+if string.find(Poly2, "x"):
+    pe = sympy.simplify(p2)
+    pe_exp = pe.expand()
+    resultFactorp2 = pe_exp.factor()
+    resuligualCerop2 = solve(pe, 'x')
 
-	print"     El segundo polinomio factorizado es: ", resultFactorp2
-	print"     El segundo polinomio igulado a cero da como raices de <x>:: ", resuligualCerop2
-	print""
-	print""
+    print"     El segundo polinomio factorizado es: ", resultFactorp2
+    print"     El segundo polinomio igulado a cero da como raices de <x>:: ", resuligualCerop2
+    print""
+    print""
 
-
-if string.find(resultMult, "x"):
 
 # Factoriza el resultado de la multiplicacion y raiz
 
-	pm = sympy.simplify(mult(Poly1, Poly2))
-	pm_exp = pm.expand()
-	resultFactorpm = pm_exp.factor()
-	resuligualCeropm = solve(pm, 'x')
+if string.find(resultMult, "x"):
+    pm = sympy.simplify(mult(p1, p2))
+    pm_exp = pm.expand()
+    resultFactorpm = pm_exp.factor()
+    resuligualCeropm = solve(pm, 'x')
 
-	print"     El resultado de la multiplicacion factorizado es: ", resultFactorpm
-	print"     El resultado de la multiplicacion igulado a cero da como raices de <x>: ", resuligualCeropm
-	print""
-	print""
+    print"     El resultado de la multiplicacion factorizado es: ", resultFactorpm
+    print"     El resultado de la multiplicacion igulado a cero da como raices de <x>: ", resuligualCeropm
+    print""
+    print""
 
-
-if string.find(resultSuma, "x"):
 
 # Factoriza el resultado de la suma y raiz
 
-	ps = sympy.simplify(suma(Poly1, Poly2))
-	ps_exp = ps.expand()
-	resultFactorps = ps_exp.factor()
-	resuligualCerops = solve(ps, 'x')	
-	
-	print"     El resultado de la suma factorizado es: ", resultFactorps
-	print"     El resultado de la suma igulado a cero da como raices de <x>: ", resuligualCerops
-	print""
-	print""
+if string.find(resultSuma, "x"):
+    pis = sympy.simplify(suma(p1, p2))
+    pis_exp = pis.expand()
+    resultFactorps = pis_exp.factor()
+    resuligualCerops = solve(pis, 'x')
 
+    print"     El resultado de la suma factorizado es: ", resultFactorps
+    print"     El resultado de la suma igulado a cero da como raices de <x>: ", resuligualCerops
+    print""
+    print""
 
-if string.find(resultRes, "x"):
 
 # Factoriza el resultado de la resta y raiz
 
-	pr = sympy.simplify(resultRes)
-	pr_exp = pr.expand()
-	resultFactorpr = pr_exp.factor()
-	resuligualCeropr = solve(pr, 'x')
+if string.find(resultRes, "x"):
+    pr = sympy.simplify(res(p1, p2))
+    pr_exp = pr.expand()
+    resultFactorpr = pr_exp.factor()
+    resuligualCeropr = solve(pr, 'x')
 
-	print"     El resultado de la resta factorizado es: ", resultFactorpr
-	print"     El resultado de la resta igulado a cero da como raices de <x>: ", resuligualCeropr
-	print""
-	print""
+    print"     El resultado de la resta factorizado es: ", resultFactorpr
+    print"     El resultado de la resta igulado a cero da como raices de <x>: ", resuligualCeropr
+    print""
+    print""
 
-
-if string.find(resultDiv, "x"):
 
 # Factoriza el resultado de la division y raiz
 
-	pd = sympy.simplify(C)
-	pd_exp = pd.expand()
-	resultFactorpd = pd_exp.factor()
-	resuligualCeropd = solve(pd, 'x')
+if string.find(resultDiv, "x"):
+    pd = sympy.simplify(C)
+    pd_exp = pd.expand()
+    resultFactorpd = pd_exp.factor()
+    resuligualCeropd = solve(pd, 'x')
 
-	print"     El resultado de la division factorizado es: ", resultFactorpd
-	print"     El resultado de la division igulado a cero da como raices de <x>: ", resuligualCeropd
+    print"     El resultado de la division factorizado es: ", resultFactorpd
+    print"     El resultado de la division igulado a cero da como raices de <x>: ", resuligualCeropd
 
 else:
-	print""
-	print""
-
-# previene el cierre de la consola en algunos equipos	
-	raw_input("     Fin del programa ........... Presione una tecla para salir")
+    print""
+    print""
+    print"     Fin del programa ..........."
 
 
+print""
+print""
 
-
+raw_input("         presione <ENTER> para salir del programa ---------->>>>"))
